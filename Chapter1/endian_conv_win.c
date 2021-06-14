6c750b716588cd77e84c46a80ed654744acee374
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-06-14 17:37:22 +0900

    6/14 커밋

---
+++ Chapter1/endian_conv_win.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <WinSock2.h>
+void ErrorHnadling(char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	unsigned short host_port = 0x1234;
+	unsigned short net_port;
+	unsigned long host_addr = 0x12345678;
+	unsigned long net_addr;
+
+	return 0;
+}

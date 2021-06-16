78ab75c4d64e1e35dcf3cce059e4ef923984dc8a
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-06-16 17:26:37 +0900

    6/16 커밋

--- TCP-IP/remove_zombie.c
+++ TCP-IP/remove_zombie.c
@@ -11,6 +11,48 @@
 //	if (WIFEXITED(status))
 //	{
 //		printf("Removed proc id : %d \n", id);
-//		printf("Child send: %d \n", WEXITSTATUS(status);
+//		printf("Child send: %d \n", WEXITSTATUS(status));
 //	}
+//}
+//
+//int main(int argc, char* argv[])
+//{
+//	pid_t pid;
+//	struct sigaction act;
+//	act.sa_handler = read_childproc;
+//	sigemptyset(&act.sa_mask);
+//	act.sa_flags = 0;
+//	sigaction(SIGCHLD, &act, 0);
+//
+//	pid = fork();
+//	if (pid==0)
+//	{
+//		puts("Hi! I'm child process");
+//		sleep(10);
+//		return 12;
+//	}
+//	else
+//	{
+//		printf("Child proc id: %d \n", pid);
+//		pid = fork();
+//		if (pid==0)
+//		{
+//			puts("Hi! I'm child process");
+//			sleep(10);
+//			exit(24);
+//		}
+//		else
+//		{
+//			int i;
+//			printf("Child proc id: %d", pid);
+//			for (i = 0; i < 5; i++)
+//			{
+//				puts("wait....");
+//				sleep(5);
+//			}
+//		}
+//	}
+//
+//
+//	return 0;
 //}
